bf7da34b6a28ce10f85088b2637be5ad1e444a7f
Zisheng Ye <ye57@wisc.edu>
2024-02-24 20:33:21 -0600

    add docker repo

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@
 sudo usermod -aG docker $USER
 newgrp docker
 
+docker pull zishengy/nodecloud:latest
+
 git clone https://github.com/zishengye/NodeCloud.git
 
 git config --global user.email "zisheng_ye@outlook.com"
